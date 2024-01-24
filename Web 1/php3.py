ca9456cc7e0aeb6b70ad2e9e1c4b79ac42e9b2b6
Abhishek Kumar <59302147+2Abhi000@users.noreply.github.com>
2024-01-24 22:12:07 +0530

    Add files via upload

---
+++ Web 1/php3.py
@@ -0,0 +1,150 @@
+import requests
+
+cookies = {
+    'PMBC': '9900e0795f3ff4e57230bf97b0c7e4d0',
+    'xf_csrf': 'vgPsCgaDmn15i5F-',
+}
+
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Connection': 'keep-alive',
+    # 'Cookie': 'PMBC=9900e0795f3ff4e57230bf97b0c7e4d0; xf_csrf=vgPsCgaDmn15i5F-',
+    'Referer': 'https://altenens.is/award-system/list',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+}
+
+response = requests.get('https://altenens.is/ads.html', cookies=cookies, headers=headers)
+print(response.text)
+
+#Output - content file"
+'''
+<p>Ads In Altenen Team</p>
+<h1 id="mcetoc_1cfknt75o0" align="center"><span style="font-weight: 400; background-color: #000000;"> <span style="font-family: Comic Sans MS;"><span style="color: #ffffff;">Why Advertise With</span> <span style="color: #ff0000;">A</span><span style="color: #ffffff;">T</span><span style="color: #ff0000;">N</span> <span style="color: #ffffff;">?</span></span></span></h1>
+<p align="center"><span style="color: #800000; font-family: Comic Sans MS;">Altenens.com is quickly becoming the web's premier financial destination. We have reached over 500,000 members in less than 5 short years, and are viewed by over 500,000 unique visitors daily. ATN&nbsp; is in a perfect position for it's second stage of exponential growth, and is taking the necessary steps to achieve Internet notoriety. </span></p>
+<p align="center"><span style="color: #008000; font-family: Comic Sans MS;"> <span style="background-color: #ffff00;">Site Info</span></span></p>
+<p align="center">&nbsp;</p>
+<p><span style="color: #800000; font-family: Comic Sans MS;">Our mission: To develop a web-environment that fosters true wealth by providing education, intelligent discussion, and access to real-world results through people who consistently achieve them.</span></p>
+<p><span style="color: #800000; font-family: Comic Sans MS;">ATN is still very much in it's infancy. However, we are working hard around the clock to fulfill this mission, and turn what once was a dream, into a brilliant reality. While these goals are simple in concept, they will one day have monumental meaning to the lives we positively impact.</span></p>      
+<p><span style="color: #800000; font-family: Comic Sans MS;">Allow us to make a positive impact on your business. Advertise on ATN&nbsp; Today!</span></p>
+<h1 id="mcetoc_1cfknt75p1"><span style="font-weight: 400;"> <span style="color: #008000; font-family: Comic Sans MS; font-size: large;">Price Charts</span></span></h1>
+<p><span style="color: #800000; font-family: Comic Sans MS; font-size: large;">Below is our Advertising Media Kit. We also offer bulk discounts for larger purchases, package deals for running multiple ads, and custom solutions for those looking for something unique. If you don't see what you're looking for below, please contact us!</span></p>
+<p><span style="font-family: Comic Sans MS; font-size: large;"><span style="font-family: Comic Sans MS; font-size: large;"><br /> To Purchase Advertising or Enquire about Advertising:<br /> Contact Us at<span style="font-family: Comic Sans MS; font-size: large;">:&nbsp; <a href="https://altenens.org/index.php?conversations/add&to=T3eS" target="_blank" rel="noopener noreferrer">https://altenens.org/index.php?conversations/add&to=T3eS</a></span></span></span></p>
+<p><span style="color: #ff0000; font-family: Comic Sans MS; font-size: large;">Please Keep your Eyes from scammers and fake guys .. i never contact or add anyone so don't send money to anyone before he send you pm from the admin username T3eS</span></p>
+<p><span style="color: #0000ff; font-family: Comic Sans MS;"> =================================================================</span></p>
+<p style="color: #000000; font-family: Times New Roman; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;"><span style="font-family: Comic Sans MS; font-size: large;"><span style="color: #ff0000;">468</span><span style="color: red;">X64 Small Premier Leader board (Ad is in a Header)</span></span></p>
+<table style="font-family: Times New Roman; letter-spacing: normal; orphans: auto; text-indent: 0px; text-transform: none; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;" border="1" cellspacing="3">
+<tbody>
+<tr>
+<td><span style="font-family: Comic Sans MS;">Time Period</span></td>
+<td><span style="font-family: Comic Sans MS;">Price</span></td>
+</tr>
+<tr>
+<td><span style="font-family: Comic Sans MS;">1 Month</span></td>
+<td><span style="font-family: Comic Sans MS;">$500</span></td>
+</tr>
+</tbody>
+</table>
+<p style="color: #000000; font-family: Times New Roman; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;"><span style="color: #008000;"><strong>Offer: get 3 months and get discount with 200$ you will pay only 1300$</strong></span></p>
+<p style="color: #000000; font-family: Times New Roman; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;"><span style="color: #0000ff; font-family: Comic Sans MS;"> =================================================================</span></p>
+<p><span style="font-family: Comic Sans MS; font-size: large;"><span style="font-family: Comic Sans MS; font-size: large;"><span style="font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; -webkit-text-stroke-width: 0px; color: red; font-family: Comic Sans MS;"> 900X120</span><span style="font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; -webkit-text-stroke-width: 0px; color: red; font-family: Comic Sans MS; font-size: large;"><span class="Apple-converted-space">&nbsp;</span>Big Premier Leader board (Ad is in a Header)</span></span></span></p>
+<table style="font-family: Times New Roman; letter-spacing: normal; orphans: auto; text-indent: 0px; text-transform: none; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;" border="1" cellspacing="3">
+<tbody>
+<tr>
+<td><span style="font-family: Comic Sans MS;">Time Period</span></td>
+<td><span style="font-family: Comic Sans MS;">Price</span></td>
+</tr>
+<tr>
+<td><span style="font-family: Comic Sans MS;">1 Month</span></td>
+<td><span style="font-family: Comic Sans MS;">$1000</span></td>
+</tr>
+</tbody>
+</table>
+<p style="color: #000000; font-family: Times New Roman; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;"><span style="color: #008000;"><strong>Offer: get 3 months and get discount with 300$ you will pay only 2700$</strong></span></p>
+<p style="color: #000000; font-family: Times New Roman; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;"><span style="color: #0000ff; font-family: Comic Sans MS;"> =================================================================</span></p>
+<p><span style="font-family: Comic Sans MS; font-size: large;"><span style="font-family: Comic Sans MS; font-size: large;"><span style="font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; -webkit-text-stroke-width: 0px; color: red; font-family: Comic Sans MS;"> 900X240</span><span style="font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; -webkit-text-stroke-width: 0px; color: red; font-family: Comic Sans MS; font-size: large;"><span class="Apple-converted-space">&nbsp;</span>Biggest&nbsp; Premier Leader board (Ad is in a Header)</span></span></span></p>
+<table style="font-family: Times New Roman; letter-spacing: normal; orphans: auto; text-indent: 0px; text-transform: none; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;" border="1" cellspacing="3">
+<tbody>
+<tr>
+<td><span style="font-family: Comic Sans MS;">Time Period</span></td>
+<td><span style="font-family: Comic Sans MS;">Price</span></td>
+</tr>
+<tr>
+<td><span style="font-family: Comic Sans MS;">1 Month</span></td>
+<td><span style="font-family: Comic Sans MS;">$1500</span></td>
+</tr>
+</tbody>
+</table>
+<p style="color: #000000; font-family: Times New Roman; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;"><span style="color: #008000;"><strong>Offer: get 3 months and get discount with 500$ you will pay only 4000$</strong></span></p>
+<p style="color: #000000; font-family: Times New Roman; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;"><span style="color: #0000ff; font-family: Comic Sans MS;"> =================================================================</span></p>
+<p style="color: #000000; font-family: Times New Roman; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;"><span style="font-family: Comic Sans MS;"><br /> <span style="color: red;">Sticky Threads</span></span></p>
+<table style="font-family: Times New Roman; letter-spacing: normal; orphans: auto; text-indent: 0px; text-transform: none; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;" border="1" cellspacing="3">
+<tbody>
+<tr>
+<td><span style="font-family: Comic Sans MS;">Topic Section</span></td>
+<td><span style="font-family: Comic Sans MS;">Weekly Price</span></td>
+<td><span style="font-family: Comic Sans MS;">Monthly Price</span></td>
+</tr>
+<tr>
+<td><span style="font-family: Comic Sans MS;">I Sell Open Source</span></td>
+<td><span style="font-family: Comic Sans MS;">$100</span></td>
+<td><span style="font-family: Comic Sans MS;">$350</span></td>
+</tr>
+<tr>
+<td><span style="font-family: Comic Sans MS;">Accounts and Database Dumps</span></td>
+<td><span style="font-family: Comic Sans MS;">$200</span></td>
+<td><span style="font-family: Comic Sans MS;">$750</span></td>
+</tr>
+</tbody>
+</table>
+<table style="font-family: Times New Roman; letter-spacing: normal; orphans: auto; text-indent: 0px; text-transform: none; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;" border="1" cellspacing="3">
+<tbody>
+<tr>
+<td><span style="font-family: Comic Sans MS;">I Sell Verified Carders&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></td>
+<td><span style="font-family: Comic Sans MS;">$300&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></td>
+<td><span style="font-family: Comic Sans MS;">$500&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></td>
+</tr>
+</tbody>
+</table>
+<p><span style="font-family: Comic Sans MS; font-size: large;"><span style="font-family: Comic Sans MS; font-size: large;"><span style="font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; -webkit-text-stroke-width: 0px; color: #0000ff; font-family: Comic Sans MS;"> =================================================================</span></span></span></p>
+<p style="color: #000000; font-family: Times New Roman; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;"><span style="color: #ff0000; font-family: Comic Sans MS;">Send Emails to all Members in ATN [Around 500k Members] Onetime emails sent via admin Cp</span></p>
+<table style="font-family: Times New Roman; letter-spacing: normal; orphans: auto; text-indent: 0px; text-transform: none; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;" border="1" cellspacing="3">
+<tbody>
+<tr>
+<td><span style="font-family: Comic Sans MS;">Time Period</span></td>
+<td><span style="font-family: Comic Sans MS;">Price</span></td>
+</tr>
+<tr>
+<td><span style="font-family: Comic Sans MS;">Onetime</span></td>
+<td><span style="font-family: Comic Sans MS;">$10,000</span></td>
+</tr>
+</tbody>
+</table>
+<p style="color: #000000; font-family: Times New Roman; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;"><span style="color: #008000;"><strong>You will give us what do you want and we will sent it to all our members emails we have about 200k members in ATN</strong></span></p>
+<p style="color: #000000; font-family: Times New Roman; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;"><span style="color: #0000ff; font-family: Comic Sans MS;"> =================================================================</span></p>
+<p style="color: #000000; font-family: Times New Roman; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;"><span style="color: #ff0000; font-family: Comic Sans MS;">Send Privet Massage to all member in ATN Forum [more thank 200k Mmbers]</span></p>
+<table style="font-family: Times New Roman; letter-spacing: normal; orphans: auto; text-indent: 0px; text-transform: none; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;" border="1" cellspacing="3">
+<tbody>
+<tr>
+<td><span style="font-family: Comic Sans MS;">Time Period</span></td>
+<td><span style="font-family: Comic Sans MS;">Price</span></td>
+</tr>
+<tr>
+<td><span style="font-family: Comic Sans MS;">Onetime</span></td>
+<td><span style="font-family: Comic Sans MS;">$5,000</span></td>
+</tr>
+</tbody>
+</table>
+<p style="color: #000000; font-family: Times New Roman; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;"><strong><span style="color: #008000;">You will give us what do you want to say and we will send Pm to all members in ATN&nbsp; via Admin cp</span></strong></p>
+<p style="color: #000000; font-family: Times New Roman; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;"><span style="color: #0000ff; font-family: Comic Sans MS;"> =================================================================</span></p>
+<p style="color: #000000; font-family: Times New Roman; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;"><strong><span style="color: #008080;">Note: We accept Only Bitcoin As Payment Method.</span></strong></p>
+<p style="color: #000000; font-family: Times New Roman; font-size: medium; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: normal; orphans: auto; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px;" align="center">&nbsp;</p>
+'''
