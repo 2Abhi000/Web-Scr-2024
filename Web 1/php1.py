66083a4032c9b98ab1a6242888609500a42afa28
Abhishek Kumar <59302147+2Abhi000@users.noreply.github.com>
2024-01-24 21:51:23 +0530

    Add files via upload

---
+++ Web 1/php1.py
@@ -0,0 +1,61 @@
+import requests
+
+cookies = {
+    'PMBC': '9900e0795f3ff4e57230bf97b0c7e4d0',
+    'xf_csrf': 'vgPsCgaDmn15i5F-',
+}
+
+headers = {
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    # 'Cookie': 'PMBC=9900e0795f3ff4e57230bf97b0c7e4d0; xf_csrf=vgPsCgaDmn15i5F-',
+    'Origin': 'https://altenens.is',
+    'Referer': 'https://altenens.is/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+}
+
+data = {
+    'tab_id': '0',
+    'modern_statistic_id': '1',
+    'hard_reload': 'false',
+    '_xfRequestUri': '/',
+    '_xfWithData': '1',
+    '_xfToken': '1706112456,c4ce0211c1278db7d7f48be8befb0dfe',
+    '_xfResponseType': 'json',
+}
+
+response = requests.post('https://altenens.is/index.php?brms-statistics/load-tab', cookies=cookies, headers=headers, data=data)
+print(response.text)
+
+#Output - TabContentHtml file"
+'''
+{
+    "tabContentHtml": "\n\t<ol class=\"brmsContentList u-clearFix\">\n\t\t\n\t\t\t\n\t\t\t\n\t
+    <li class=\"itemStast BRMSToolTip itemThread first\">\n\t\t
+    <div class=\"itemContent\">\n\t\t\t\n\t<div class=\"listBlock counter counter_1\">\n\t\t
+    <span class=\"countNumber\">1</span>\n\t\t<span class=\"arrow\"><span></span></span>\n\t</div>\n\t
+    <div class=\"listBlock itemTitle\">\n\t\t\n\t\t<a href=\"/threads/mega-hot-arab-model-yasmeena-leaked.2183991/\"class=\"\"\n\t\t\tdata-tp-primary=\"on\" data-xf-init=\"brms-tooltip\"\n\t\t\t\n\t\t\tdata-kind=\"thread\"\n\t\t\tdata-box=\"Images &amp;  Videos &amp; Porn Accounts\ud83d\udd1e\"\n\t\t\tdata-view=\"1186\"\n\t\t\tdata-rep=\"192\"\n\t\t\tdata-like=\"185\"\n\t\t\t\n\t\t\tdata-preview-url=\"\"> [MEGA] HOT ARAB MODEL YASMEENA LEAKED</a>\n\t
+    </div>\n\n\t\t\t\n\t\t\t\t<div class=\"listBlock itemDetail itemDetailDate\">\n\t\t\t\t\t
+    <a class=\"paint\" href=\"/threads/mega-hot-arab-model-yasmeena-leaked.2183991/\" title=\"\"><time  class=\"u-dt\" dir=\"auto\" datetime=\"2024-01-24T16:07:46+0000\" data-time=\"1706112466\" data-date-string=\"Jan 24, 2024\" data-time-string=\"4:07 PM\" title=\"Jan 24, 2024 at 4:07 PM\">1 minute ago</time></a>
+    \n\t\t\t\t</div>\n\t\t\t\t<div class=\"listBlock itemDetail itemDetailName\">\n\t\t\t\t\t<a href=\"/members/ghostlygamer.1352519/\" class=\"username \" dir=\"auto\" data-user-id=\"1352519\" data-xf-init=\"member-tooltip\"><span class=\"username--style29\">GhostlyGamer</span></a>\n\t\t\t\t</div>\n\t\t\t\n\t\t</div>
+    \n\t</li>\n\n\t\t\n\t\t\t\n\t\t\t\n\t<li class=\"itemStast BRMSToolTip itemThread\">\n\t\t<div class=\"itemContent\">\n\t\t\t\n\t<div class=\"listBlock counter counter_2\">\n\t\t<span class=\"countNumber\">2</span>\n\t\t<span class=\"arrow\"><span></span></span>\n\t</div>\n\t<div class=\"listBlock itemTitle\">\n\t\t\n\t\t<a href=\"/threads/teen-girls-private-photo-ultra-rare-leaks-mega-hot.2197557/\" class=\"\"\n\t\t\tdata-tp-primary=\"on\" data-xf-init=\"brms-tooltip\"\n\t\t\t\n\t\t\tdata-kind=\"thread\"\n\t\t\tdata-box=\"Images &amp;  Videos &amp; Porn Accounts\ud83d\udd1e\"\n\t\t\tdata-view=\"517\"\n\t\t\tdata-rep=\"69\"\n\t\t\tdata-like=\"69\"\n\t\t\t\n\t\t\tdata-preview-url=\"\">
+      TEEN GIRLS PRIVATE PHOTO ULTRA RARE LEAKS [MEGA] [HOT]</a>\n\t</div>\n\n\t\t\t\n\t\t\t\t<div class=\"listBlock itemDetail itemDetailDate\">\n\t\t\t\t\t<a class=\"paint\" href=\"/threads/teen-girls-private-photo-ultra-rare-leaks-mega-hot.2197557/\" title=\"\"><time  class=\"u-dt\" dir=\"auto\" datetime=\"2024-01-24T16:07:45+0000\" data-time=\"1706112465\" data-date-string=\"Jan 24, 2024\" data-time-string=\"4:07 PM\" title=\"Jan 24, 2024 at 4:07 PM\">1 minute ago</time></a>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"listBlock itemDetail itemDetailName\">\n\t\t\t\t\t<a href=\"/members/ghostlygamer.1352519/\" class=\"username \" dir=\"auto\" data-user-id=\"1352519\" data-xf-init=\"member-tooltip\"><span class=\"username--style29\">GhostlyGamer</span></a>\n\t\t\t\t</div>\n\t\t\t\n\t\t</div>\n\t</li>\n\n\t\t\n\t\t\t\n\t\t\t\n\t
+      <li class=\"itemStast BRMSToolTip itemThread\">\n\t\t<div class=\"itemContent\">\n\t\t\t\n\t<div class=\"listBlock counter counter_3\">\n\t\t<span class=\"countNumber\">3</span>\n\t\t<span class=\"arrow\"><span></span></span>\n\t</div>\n\t<div class=\"listBlock itemTitle\">\n\t\t\n\t\t<a href=\"/threads/%E2%9D%A4%EF%B8%8Fnetflix-premium%E2%9D%A4%EF%B8%8Fx46-fresh-accounts%E2%9D%A4%EF%B8%8Fno-capture%E2%9D%A4%EF%B8%8Fhurry-up%E2%9D%A4%EF%B8%8Fgrab-now%E2%9D%A4%EF%B8%8F.2198527/\" class=\"\"\n\t\t\tdata-tp-primary=\"on\" data-xf-init=\"brms-tooltip\"\n\t\t\t\n\t\t\tdata-kind=\"thread\"\n\t\t\tdata-box=\"Netflix\"\n\t\t\tdata-view=\"309\"\n\t\t\tdata-rep=\"24\"\n\t\t\tdata-like=\"25\"\n\t\t\t\n\t\t\tdata-preview-url=\"\"> \u2764\ufe0fNETFLIX PREMIUM\u2764\ufe0fX46 FRESH ACCOUNTS\u2764\ufe0fNO CAPTURE\u2764\ufe0fHURRY UP\u2764\ufe0fGRAB NOW\u2764\ufe0f</a>\n\t</div>\n\n\t\t\t\n\t\t\t\t<div class=\"listBlock itemDetail itemDetailDate\">\n\t\t\t\t\t<a class=\"paint\" href=\"/threads/%E2%9D%A4%EF%B8%8Fnetflix-premium%E2%9D%A4%EF%B8%8Fx46-fresh-accounts%E2%9D%A4%EF%B8%8Fno-capture%E2%9D%A4%EF%B8%8Fhurry-up%E2%9D%A4%EF%B8%8Fgrab-now%E2%9D%A4%EF%B8%8F.2198527/\" title=\"\"><time  class=\"u-dt\" dir=\"auto\" datetime=\"2024-01-24T16:07:38+0000\" data-time=\"1706112458\" data-date-string=\"Jan 24, 2024\" data-time-string=\"4:07 PM\" title=\"Jan 24, 2024 at 4:07 PM\">
+      1 minute ago</time></a>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"listBlock itemDetail itemDetailName\">\n\t\t\t\t\t<a href=\"/members/ghostlygamer.1352519/\" class=\"username \" dir=\"auto\" data-user-id=\"1352519\" data-xf-init=\"member-tooltip\"><span class=\"username--style29\">GhostlyGamer</span></a>\n\t\t\t\t</div>\n\t\t\t\n\t\t</div>\n\t</li>\n\n\t\t\n\t\t\t\n\t\t\t\n\t<li class=\"itemStast BRMSToolTip itemThread\">\n\t\t<div class=\"itemContent\">\n\t\t\t\n\t<div class=\"listBlock counter counter_4\">\n\t\t<span class=\"countNumber\">4</span>\n\t\t<span class=\"arrow\"><span></span></span>\n\t</div>\n\t<div class=\"listBlock itemTitle\">\n\t\t\n\t\t<a href=\"/threads/mega-%E2%AD%90%E2%9D%A3%EF%B8%8Fnew-homemade-porn-pack-08%E2%AD%90%E2%9D%A3%EF%B8%8F10-in-1%E2%AD%90%E2%9D%A3%EF%B8%8Fgrab-asap%E2%9D%A3%EF%B8%8F%E2%AD%90link-working-rn%E2%9C%94%EF%B8%8F.2198205/\" class=\"\"\n\t\t\tdata-tp-primary=\"on\" data-xf-init=\"brms-tooltip\"\n\t\t\t\n\t\t\tdata-kind=\"thread\"\n\t\t\tdata-box=\"Images &amp;  Videos &amp; Porn Accounts\ud83d\udd1e\"\n\t\t\tdata-view=\"343\"\n\t\t\tdata-rep=\"28\"\n\t\t\tdata-like=\"29\"\n\t\t\t\n\t\t\tdata-preview-url=\"\"> [MEGA]
+        \u2b50\u2763\ufe0fNEW HOMEMADE PORN PACK 08\u2b50\u2763\ufe0f10 IN 1\u2b50\u2763\ufe0fGRAB ASAP\u2763\ufe0f\u2b50LINK WORKING RN\u2714\ufe0f</a>\n\t</div>\n\n\t\t\t\n\t\t\t\t<div class=\"listBlock itemDetail itemDetailDate\">\n\t\t\t\t\t<a class=\"paint\" href=\"/threads/mega-%E2%AD%90%E2%9D%A3%EF%B8%8Fnew-homemade-porn-pack-08%E2%AD%90%E2%9D%A3%EF%B8%8F10-in-1%E2%AD%90%E2%9D%A3%EF%B8%8Fgrab-asap%E2%9D%A3%EF%B8%8F%E2%AD%90link-working-rn%E2%9C%94%EF%B8%8F.2198205/\" title=\"\"><time  class=\"u-dt\" dir=\"auto\" datetime=\"2024-01-24T16:07:18+0000\" data-time=\"1706112438\" data-date-string=\"Jan 24, 2024\" data-time-string=\"4:07 PM\" title=\"Jan 24, 2024 at 4:07 PM\">1 minute ago</time></a>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"listBlock itemDetail itemDetailName\">\n\t\t\t\t\t<a href=\"/members/ghostlygamer.1352519/\" class=\"username \" dir=\"auto\" data-user-id=\"1352519\" data-xf-init=\"member-tooltip\">
+      <span class=\"username--style29\">GhostlyGamer</span></a>\n\t\t\t\t</div>\n\t\t\t\n\t\t</div>\n\t</li>\n\n\t\t\n\t\t\t\n\t\t\t\n\t<li class=\"itemStast BRMSToolTip itemThread\">\n\t\t<div class=\"itemContent\">\n\t\t\t\n\t<div class=\"listBlock counter counter_5\">\n\t\t<span class=\"countNumber\">5</span>\n\t\t<span class=\"arrow\"><span></span></span>\n\t</div>\n\t<div class=\"listBlock itemTitle\">\n\t\t\n\t\t<a href=\"/threads/%F0%9F%94%9E%F0%9F%92%8B-x69-porn-brazzer-accounts-%F0%9F%94%A5%F0%9F%94%A5.1931549/\" class=\"\"\n\t\t\tdata-tp-primary=\"on\" data-xf-init=\"brms-tooltip\"\n\t\t\t\n\t\t\tdata-kind=\"thread\"\n\t\t\tdata-box=\"Brazzers Accounts\"\n\t\t\tdata-view=\"6543\"\n\t\t\tdata-rep=\"558\"\n\t\t\tdata-like=\"284\"\n\t\t\t\n\t\t\tdata-preview-url=\"\"><span class=\"label label--green\" dir=\"auto\">Account</span><span class=\"label-append\">&nbsp;</span> \ud83d\udd1e\ud83d\udc8b X69 PORN BRAZZER ACCOUNTS \ud83d\udd25\ud83d\udd25</a>\n\t</div>\n\n\t\t\t\n\t\t\t\t<div class=\"listBlock itemDetail itemDetailDate\">\n\t\t\t\t\t<a class=\"paint\" href=\"/threads/%F0%9F%94%9E%F0%9F%92%8B-x69-porn-brazzer-accounts-%F0%9F%94%A5%F0%9F%94%A5.1931549/\" title=\"\"><time  class=\"u-dt\" dir=\"auto\" datetime=\"2024-01-24T16:07:17+0000\" data-time=\"1706112437\" data-date-string=\"Jan 24, 2024\" data-time-string=\"4:07 PM\" title=\"Jan 24, 2024 at 4:07 PM\">
+      1 minute ago</time></a>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"listBlock itemDetail itemDetailName\">\n\t\t\t\t\t<a href=\"/members/esugtfc333.1273978/\" class=\"username \" dir=\"auto\" data-user-id=\"1273978\" data-xf-init=\"member-tooltip\">esugtfc333</a>\n\t\t\t\t</div>\n\t\t\t\n\t\t</div>\n\t</li>\n\n\t\t\n\t</ol>\n",
+    "limit": 15,
+    "tabId": 0
+}
+
+'''
